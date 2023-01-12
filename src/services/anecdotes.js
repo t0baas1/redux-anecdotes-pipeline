0271715212eabf412cab0b5d328d393b5bc89886
Topi Kanninen <topi.kanninen@sci.fi>
2023-01-12 16:31:57 +0200

    deployed server outside of repo

--- src/services/anecdotes.js
+++ src/services/anecdotes.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3002/anecdotes'
+const baseUrl = 'https://fake-server-anec.herokuapp.com/anecdotes'
 
 const getAll = async () => {
   const response = await axios.get(baseUrl)
