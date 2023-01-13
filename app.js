abe3201fdd2ffb2b69c8d149304a8600e3b83bad
Topi Kanninen <topi.kanninen@sci.fi>
2023-01-13 13:34:50 +0200

    added server

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+
+// Heroku dynamically sets a port
+const PORT = process.env.PORT || 5000
+
+app.use(express.static('build'))
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.listen(PORT, () => {
+
+  // eslint-disable-next-line no-console
+  console.log('server started on port 5000')
+})
