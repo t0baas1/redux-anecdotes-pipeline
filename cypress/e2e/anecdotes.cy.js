2f891b28d3afb39579563792230eb5e42af94044
Topi Kanninen <topi.kanninen@sci.fi>
2023-01-13 12:56:40 +0200

    added e2e tests

---
+++ cypress/e2e/anecdotes.cy.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-undef */
+import moment from 'moment'
+describe('Anecdotes', () => {
+
+  const second = moment().get('minute')
+  const anecdote = `Cypress test anecdote ${second}`
+
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+    cy.intercept('GET', '**/anecdotes').as('getAnecs')
+  })
+
+  it('front page can be opened', () => {
+    cy.contains('Anecdotes')
+    cy.contains('create new')
+  })
+
+  it('can filter out anecdotes', () => {
+    cy.wait('@getAnecs')
+    cy.contains('If it hurts, do it more often').should('exist')
+    cy.get('input').eq(0).type('xxxx').blur()
+    cy.contains('If it hurts, do it more often').should('not.exist')
+    cy.get('input').eq(0).clear().blur()
+    cy.contains('If it hurts, do it more often').should('exist')
+  })
+
+  it('can create new anecdote', () => {
+    cy.wait('@getAnecs')
+    cy.get('input').eq(1).type(anecdote).blur()
+    cy.get('button').contains('create').click()
+    cy.wait(1000)
+    cy.contains(anecdote)
+  })
+})
